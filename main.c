073392bfe2db4e62bc049d207e87b66532ebeeed
Katya Feldman <katyapinich@gmail.com>
2020-04-04 16:40:33 +0300

    Searching in multiple lines

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "command_line_parser.h"
 
 void Grep(Parameters *parameters);
-char* ReadLineFromFile(const char *filename);
-char* ReadLine(Parameters *parameters);
+char* ReadLineFromFile(const char *filename, int start_from);
+char* ReadLine(Parameters *parameters, int read_from);
 bool IsMatchInLine(const char *expression, const char *line);
 
 int main(int argc, char *argv[])
@@ -22,39 +24,52 @@ int main(int argc, char *argv[])
 void Grep(Parameters *parameters)
 {
     char *line;
+    int bytes_read = 0;
 
-    line = ReadLine(parameters);
-    printf("Read Line: %s\n", line);
-
-    if (IsMatchInLine(parameters->expression, line))
+    line = ReadLine(parameters, bytes_read);
+    while (line != NULL)
     {
-        printf("%s\n", line);
+        //printf("Read line: %s", line);
+        if (IsMatchInLine(parameters->expression, line))
+        {
+            printf("%s", line);
+        }
+
+        bytes_read += strlen(line);
+        line = ReadLine(parameters, bytes_read);
     }
 }
 
-char* ReadLine(Parameters *parameters)
+char* ReadLine(Parameters *parameters, int read_from)
 {
     if (parameters->inputMode == INPUT_FILE)
     {
-        return ReadLineFromFile(parameters->filename);
+        return ReadLineFromFile(parameters->filename, read_from);
     }
 
     return NULL;
 }
 
-char* ReadLineFromFile(const char *filename)
+char* ReadLineFromFile(const char *filename, int start_from)
 {
     FILE *file;
-    char *line;
+    char *line = NULL;
+    size_t line_length = 0;
+    ssize_t bytes_read;
 
     file = fopen(filename, "r");
     if (file == NULL)
     {
         printf("Could not open file %s for reading.\n", filename);
-        return NULL;
+        exit(EXIT_FAILURE);
     }
 
-    fscanf(file, "%s\n", line);
+    fseek(file, start_from, SEEK_SET);
+    bytes_read = getline(&line, &line_length, file);
+    if (bytes_read == -1)
+    {
+        line = NULL;
+    }
 
     fclose(file);
 
