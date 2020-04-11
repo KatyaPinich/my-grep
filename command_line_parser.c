674e712c0500423ab0120684ab0e47edd9375233
Ross Bolotin <rossbolotin@mail.tau.ac.il>
2020-04-11 18:42:00 +0300

    updated parameters struct and ParseParameters to parse all sort of inputs. added InitializeParametersStruct func.

--- command_line_parser.c
+++ command_line_parser.c
@@ -2,40 +2,79 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "string_tools.h"
 
 Parameters* ParseParameters(int argc, char *argv[])
 {
     Parameters *parameters;
+    int i;
 
     parameters = (Parameters*)malloc(sizeof(Parameters));
     if (parameters == NULL)
         return parameters; // TODO: Maybe exit with a warning memory allocation failed
 
-    if (argc == 2)
+    InitializeParametersStruct(parameters);
+
+    for(i = 1; i < argc; i++)
     {
-        // We have only an expression and get the input from stdin
-        parameters->expression = CopyString(argv[1]);
-        parameters->inputMode = INPUT_STDIN;
-    }
-    else if (argc == 3)
-    {
-        // We have an expression and a file
-        parameters->expression = CopyString(argv[1]);
-        parameters->inputMode = INPUT_FILE;
-        parameters->filename = CopyString(argv[2]);
-        if (parameters->filename == NULL)
+        if (strcmp(argv[i], "-A") == 0)
         {
+            parameters->aParameter = atoi(argv[i + 1]);
+            i++;
+        }
+        else if (strcmp(argv[i], "-b") == 0)
+        {
+            parameters->bParameter = true;
+        }
+        else if (strcmp(argv[i], "-c") == 0)
+        {
+            parameters->cParameter = true;
+        }
+        else if (strcmp(argv[i], "-i") == 0)
+        {
+            parameters->iParameter = true;
+        }
+        else if (strcmp(argv[i], "-n") == 0)
+        {
+            parameters->nParameter = true;
+        }
+        else if (strcmp(argv[i], "-v") == 0)
+        {
+            parameters->vParameter = true;
+        }
+        else if (strcmp(argv[i], "-E") == 0)
+        {
+            parameters->eParameter = true;
+            parameters->expression = CopyString(argv[i + 1]);
+            i++;
+        }
+        else if (parameters->expression == NULL)
+        {
+            parameters->expression = CopyString(argv[i]);
+            if (parameters->expression == NULL)
+            {
+                FreeParameters(parameters);
+                exit(1);
+            }
+        }
+        else if (parameters->filename == NULL)
+        {
+            parameters->inputMode = INPUT_FILE;
+            parameters->filename = CopyString(argv[i]);
+            if (parameters->filename == NULL)
+            {
+                FreeParameters(parameters);
+                exit(1);
+            }
+        }
+        else
+        {
+            printf("usage: my-grep [-bcinvx] [-A num] [-E pattern] [pattern] [file]\n");
             FreeParameters(parameters);
             exit(1);
         }
-    }
-    else
-    {
-        printf("usage: my-grep [-bcinvx] [-A num] [-E pattern] [pattern] [file]\n");
-        FreeParameters(parameters);
-        exit(1);
     }
 
     return parameters;
@@ -54,4 +93,19 @@ void FreeParameters(Parameters *parameters)
     }
 
     free(parameters);
+}
+
+void InitializeParametersStruct(Parameters *parameters)
+{
+    parameters->filename = NULL;
+    parameters->expression = NULL;
+    parameters->inputMode = INPUT_STDIN;
+    parameters->aParameter = -1;
+    parameters->bParameter = false;
+    parameters->cParameter = false;
+    parameters->eParameter = false;
+    parameters->iParameter = false;
+    parameters->nParameter = false;
+    parameters->vParameter = false;
+    parameters->xParameter = false;
 }
--- command_line_parser.h
+++ command_line_parser.h
@@ -1,6 +1,8 @@
 #ifndef COMMAND_LINE_PARSER_H
 #define COMMAND_LINE_PARSER_H
 
+#include <stdbool.h>
+
 typedef enum
 {
     INPUT_FILE,
@@ -12,9 +14,18 @@ typedef struct Parameters
     char *expression;
     InputMode inputMode;
     char *filename;
+    int aParameter;
+    bool bParameter;
+    bool cParameter;
+    bool iParameter;
+    bool nParameter;
+    bool vParameter;
+    bool xParameter;
+    bool eParameter;
 } Parameters;
 
 Parameters* ParseParameters(int argc, char *argv[]);
 void FreeParameters(Parameters *parameters);
+void InitializeParametersStruct(Parameters *parameters);
 
 #endif
