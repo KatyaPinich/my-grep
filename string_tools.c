80fafc2f4e86ae5276a8021350be7deabdbafe25
Ross Bolotin <rossbolotin@mail.tau.ac.il>
2020-04-12 17:39:00 +0300

    dealing with parameters: -i -v -c -n -b

--- string_tools.c
+++ string_tools.c
@@ -22,14 +22,14 @@ char* CopyString(const char *source_string)
 char* ToLowercaseString(const char *source_string)
 {
     char *destination_string;
-    int source_string_length;
+    int source_string_length, i;
 
     source_string_length = strlen(source_string) + 1;
     destination_string = (char*)malloc(sizeof(char) * source_string_length);
     if (destination_string == NULL)
         return NULL;
 
-    for(int i = 0; i < source_string_length; i++)
+    for(i = 0; i < source_string_length; i++)
     {
         destination_string[i] = tolower(source_string[i]);
     }
