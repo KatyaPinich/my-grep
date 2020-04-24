2cc4be3208cf6cec5b32ce65558d4a30c71324ba
Katya Feldman <katyapinich@gmail.com>
2020-04-24 21:48:50 +0300

    Added a function to parse an expression
    Parses the expression into an array of elements.
    For now only regular characters are supported and the escape char '\' is recognized for this purpose.

--- regular_expression.c
+++ regular_expression.c
@@ -1,5 +1,67 @@
-//
-// Created by Katya Feldman on 24/04/2020.
-//
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #include "regular_expression.h"
+
+ExpressionElement* CreateExpressionElement(RegexType element_type, char expression_char);
+void FreeExpression(ExpressionElement *expression[], int element_count);
+
+ExpressionElement** ParseExpression(const char *expression_string)
+{
+    ExpressionElement **expression;
+    int expression_length;
+    int i = 0;
+    int element_count = 0;
+
+    expression_length = strlen(expression_string);
+
+    expression = calloc(expression_length, sizeof(*expression));
+    if (expression == NULL)
+        return NULL;
+
+    while (expression_string[i] != '\0')
+    {
+        if (expression_string[i] == '\\')
+        {
+            i++;
+        }
+
+        // For now we only have regular characters
+        expression[element_count] = CreateExpressionElement(REGEX_CHAR, expression_string[i]);
+        if (expression[element_count] == NULL)
+        {
+            FreeExpression(expression, element_count);
+            exit(EXIT_FAILURE);
+        }
+        element_count++;
+
+        i++;
+    }
+
+    return expression;
+}
+
+ExpressionElement* CreateExpressionElement(RegexType element_type, char expression_char)
+{
+    ExpressionElement *new_element = (ExpressionElement*)malloc(sizeof(ExpressionElement));
+    if (new_element != NULL)
+    {
+        new_element->type = element_type;
+        new_element->value = expression_char;
+    }
+
+    return new_element;
+}
+
+void FreeExpression(ExpressionElement *expression[], int element_count)
+{
+    int i;
+
+    for (i = 0; i < element_count; ++i)
+    {
+        free(expression[i]);
+    }
+
+    free(expression);
+}
--- regular_expression.h
+++ regular_expression.h
@@ -1,4 +1,20 @@
 #ifndef REGULAR_EXPRESSION_H
 #define REGULAR_EXPRESSION_H
 
+typedef enum RegexType
+{
+    REGEX_WILDCARD,
+    REGEX_OR,
+    REGEX_RANGE,
+    REGEX_CHAR
+} RegexType;
+
+typedef struct ExpressionElement
+{
+    RegexType type;
+    char value;
+} ExpressionElement;
+
+ExpressionElement** ParseExpression(const char *expression_string);
+
 #endif
