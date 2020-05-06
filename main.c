1f9daacf6ddbedf33cc3c3b306092e4e9876a32b
Katya Feldman <katyapinich@gmail.com>
2020-05-06 22:23:36 +0300

    Added ReportLines in Match Reporter module

--- main.c
+++ main.c
@@ -23,31 +23,20 @@ int main(int argc, char *argv[])
 void Grep(Parameters *parameters)
 {
   FILE *input_stream;
-  struct Node *lines_list = NULL, *line = NULL, *previousLine = NULL;
-  int match_count = 0;
+  struct Node *lines_list = NULL;
   input_stream = GetInputStream(parameters);
   if (input_stream == NULL) {
     FreeParameters(parameters);
     exit(EXIT_FAILURE);
   }
+
   MatchLines(parameters, &lines_list, input_stream);
-  line = lines_list;
-  while (line != NULL) {
-    if (ReportLine(line, parameters->invert_match)) {
-      if (!parameters->print_line_count) {
-        PrintLinesAfterContextSeparator(match_count, parameters->lines_after_context, previousLine);
-        ReportLineMatch(line, parameters);
-      }
-      match_count++;
-    }
-    previousLine = line;
-    line = line->next;
-  }
-  if (parameters->print_line_count) {
-    printf("%d\n", match_count);
-  }
+
+  ReportLines(lines_list, parameters);
+
   if (parameters->input_mode == INPUT_FILE) {
     fclose(input_stream);
   }
+
   FreeLinkedList(&lines_list);
 }
--- match_reporter.h
+++ match_reporter.h
@@ -6,8 +6,6 @@
 #include "command_line_parser.h"
 #include "linked_list.h"
 
-bool ReportLine(Node *line, bool invert_match);
-void PrintLinesAfterContextSeparator(int match_count, int lines_after_context, struct Node *previousLine);
-void ReportLineMatch(struct Node *line, Parameters *parameters);
+void ReportLines(Node *lines_list, Parameters *parameters);
 
 #endif  // MY_GREP_MATCH_REPORTER_H
